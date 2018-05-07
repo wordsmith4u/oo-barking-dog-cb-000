83fb9e0ea9e77440a4f56eeb63354f1784c3537f
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-05-07 19:22:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
   def name=(dogs_name)
     @name = dogs_name
-  end 
+  end
 
   def name
     @name
-  end 
-  
-end 
+  end
+
+end
 
 class Dog
   def bark
     puts "woof!"
-  end 
-  
-end 
+  end
+
+end
